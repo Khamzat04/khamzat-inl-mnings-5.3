8a752293c391c127dea311ee58c9f0a9910a75da
Elev <Elev@TEB125103>
2023-01-27 13:52:24 +0100

    Add project files.

---
+++ khamzat inlämnings 5.3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Uppgift_5._3_khamzat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                int[] tal = new int[3];
+                Console.WriteLine("skriv 3 tal som programmet ska  plussas med varann");
+
+                tal[0] = int.Parse(Console.ReadLine());
+                tal[1] = int.Parse(Console.ReadLine());
+                tal[2] = int.Parse(Console.ReadLine());
+
+
+                Console.WriteLine("summan av dina tal " + (tal[0] + tal[1] + tal[2]));
+
+                
+            }
+            catch
+            {
+                Console.WriteLine("du skrev inte heltal");
+                Console.ReadKey();
+            }
+
+        }
+
+    }
+}
